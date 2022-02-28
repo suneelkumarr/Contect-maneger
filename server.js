26ac8bfc3fd6400280210261ff7c590990594b3e
Suneel Kumar <sunilkumar05644@gmail.com>
2022-02-28 17:55:57 +0530

    dhjkshd

--- server.js
+++ server.js
@@ -1,43 +1,47 @@
-const express = require('express')
-const app = express()
-require('dotenv')
-const colors = require('colors')
-const connectDB = require('./config/db')
-const path = require('path')
+const express = require("express");
+const app = express();
+require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const path = require("path");
 //dotenv.config()
 //this is port
-const PORT =process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 //connect Database
-connectDB()
+connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  console.log("new build")
-    // Set static folder
-    app.use(express.static(__dirname + "/client/build"));
+if (process.env.NODE_ENV) {
+// console.log("new build")
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
-    });
-  } 
-  // else 
-  // {
-  //   app.use(express.static('client/build'));
-  
-  //   app.get('*', (req, res) =>{
-  //     res.sendFile(path.resolve(__dirname, "client/build/index.html"))
-  //   }
-  //   );
-  // }
+//   app.get("*", (req, res) => {
+//     res.sendFile("/client/build/index.html");
+//   });
 
-app.listen(PORT,()=>{
-console.log(`Server is running on ${PORT}`.yellow.underline)
-})
+app.use(express.static(__dirname + "client/build"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+}
+else
+{
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"))
+  }
+  );
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`.yellow.underline);
+});
