b3aaa5647ba450459931c6c8fb90a43f0f5b23d8
Suneel Kumar <sunilkumar05644@gmail.com>
2022-02-28 17:34:21 +0530

    shkhs;

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use('/api/contacts', require('./routes/contacts'));
 if (process.env.NODE_ENV === 'production') {
   console.log("new build")
     // Set static folder
-    app.use(express.static(__dirname + "client/build"));
+    app.use(express.static(__dirname + "/client/build"));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+      res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
     });
   } 
   // else 
