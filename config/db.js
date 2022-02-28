f3faa64b69995886715b713915b36639eac21f1c
Suneel Kumar <sunilkumar05644@gmail.com>
2022-02-28 16:33:36 +0530

    error resolve

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI.toString(), {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
